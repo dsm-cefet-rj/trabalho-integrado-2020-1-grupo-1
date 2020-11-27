cdfff9508f89126728c6d7be19c8a9f6b5d1cea7
Vinicius Tavares <53880840+N0stalgiaUltra@users.noreply.github.com>
2020-11-26 21:47:16 -0300

    scripts teste de defeito

---
+++ frontend/cypress/integration/loginJogador/loginJogador.test.js
@@ -0,0 +1,5 @@
+test('Usuário não cadastrado', () => {throw 'Not implemented yet'});
+
+test('#lgn_username existir no banco de dados', () => {throw 'Not implemented yet'});
+
+test('#lgn_password existir no banco de dados', () => {throw 'Not implemented yet'});
