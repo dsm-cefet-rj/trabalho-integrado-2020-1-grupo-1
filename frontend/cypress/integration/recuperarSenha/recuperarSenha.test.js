c2d0873c3b11f97081eb56541a4ef82d87e26955
YasminCardoso <yasmincardoso@users.noreply.github.com>
2020-11-26 21:16:00 -0300

    Projeto de casos de teste de defeito

---
+++ frontend/cypress/integration/recuperarSenha/recuperarSenha.test.js
@@ -0,0 +1,7 @@
+test('Usuário não cadastrado', () => {throw 'Not implemented yet'});
+
+test('E-mail vazio', () => {throw 'Not implemented yet'});
+
+test('Senha vazia', () => {throw 'Not implemented yet'});
+
+test('Senhas diferentes', () => {throw 'Not implemented yet'});
---
+++ frontend/cypress/integration/verPartidasComp/verPartidasComp.test.js
@@ -0,0 +1,1 @@
+test('A competição não existe', () => {throw 'Not implemented yet'});
