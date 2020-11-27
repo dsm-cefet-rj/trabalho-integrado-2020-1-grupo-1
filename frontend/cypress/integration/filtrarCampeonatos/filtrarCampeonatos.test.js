ef3f4890b48d27c818a49bf9c03ecb484c815cf8
Padu Cantuária <padu77+heroku@gmail.com>
2020-11-26 22:43:42 -0300

    Projeto de testes de defeito

--- frontend/cypress/integration/filtrarCampeonatos/filtrarCampeonatos.test.js
+++ frontend/cypress/integration/filtrarCampeonatos/filtrarCampeonatos.test.js
@@ -1,2 +1,3 @@
-test('Inscrever equipe com menos de 5 jogadores', () => {throw 'Not implemented yet'});
-test('Inscrever equipe sem competições abertas', () => {throw 'Not implemented yet'});
+test('filtrar por nível e equipes ', () => {throw 'Not implemented yet'});
+test('filtrar por nível e premiação ', () => {throw 'Not implemented yet'});
+test('filtrar por premiação e equipes ', () => {throw 'Not implemented yet'});
