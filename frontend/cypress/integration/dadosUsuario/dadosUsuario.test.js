eff0e61d0f5805e07cad52198bb1af4107d06913
Vinicius Tavares <53880840+N0stalgiaUltra@users.noreply.github.com>
2020-11-26 21:46:10 -0300

    scripts testes de defeito

---
+++ frontend/cypress/integration/dadosUsuario/dadosUsuario.test.js
@@ -0,0 +1,11 @@
+test('Usuário não cadastrado', () => {throw 'Not implemented yet'});
+
+test('#lgn_username existir no banco de dados', () => {throw 'Not implemented yet'});
+
+test('imagem não estar no formato png ou jpg', () => {throw 'Not implemented yet'});
+
+test('#signup_password conter tamanho menor que 8 caracteres', () => {throw 'Not implemented yet'});
+
+test('#confirm_password não ser igual a #signup_password', () => {throw 'Not implemented yet'});
+
+test('Não clicar na checkbox #cb_policies', () => {throw 'Not implemented yet'});
